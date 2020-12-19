54d66eefc06de4a973d03975e128f210c716cde9
quinn <maninios@163.com>
2020-12-19 17:13:05 +0800

    [SK]修复跳转到邮箱的bug

--- BaseUIWidget/Classes/SysJumpManager.swift
+++ BaseUIWidget/Classes/SysJumpManager.swift
@@ -27,7 +27,7 @@ public class SysJumpManager: NSObject {
             UIViewController.topViewController?.present(smsVC, animated: true, completion: nil)
         }
     }
-    public func sendEmail(errMsg:String,body:String,title:String,email:String)->Error{
+    public func sendEmail(rootVC: UIViewController?,errMsg:String,body:String,title:String,email:String)->Error?{
         /// TODO :
         if MFMailComposeViewController.canSendMail(){//是否可以发邮件 // 如果不能,去系统设置接收邮箱
             let mailView = MFMailComposeViewController()
@@ -35,9 +35,14 @@ public class SysJumpManager: NSObject {
             mailView.setToRecipients([email])//接收邮件的邮箱
             mailView.setSubject(title)
             mailView.setMessageBody(body, isHTML: false)
-            self.present(mailView, animated: false, completion: nil)
+            if rootVC != nil{
+                rootVC?.present(mailView, animated: true, completion: nil)
+            }else {
+                UIViewController.topViewController?.present(mailView, animated: true, completion: nil)
+            }
+            return nil
         }else{
-            return NSError(domain: "cannot sent", code: -, userInfo: [:])
+            return NSError(domain: "cannot sent", code: -1, userInfo: [:])
         }
     }
 }
@@ -46,4 +51,7 @@ extension SysJumpManager: MFMessageComposeViewControllerDelegate {
     public func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
         controller.dismiss(animated: true, completion: nil)
     }
+}
+extension SysJumpManager:MFMailComposeViewControllerDelegate{
+    
 }
