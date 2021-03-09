f164964642816d40f7a848202a0fa3dc408773e9
walker <zhangheshan@funlink-tech.com>
2021-03-09 19:58:10 +0800

    [GL][添加上拉刷新控件]

---
+++ BaseUIWidget/Classes/LFGRefresh/LFGRefreshFooter.swift
@@ -0,0 +1,13 @@
+//
+//  LFGRefreshFooter.swift
+//  CTBaseUIWidget
+//
+//  Created by walker on 2021/3/9.
+//
+
+import Foundation
+import MJRefresh
+
+public class LFGRefreshFooter: MJRefreshAutoFooter {
+    
+}
